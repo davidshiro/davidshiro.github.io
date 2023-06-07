ea7cf10daa3d726d5909115f071a77f07b09a0a1
zirconion <114958359+davidshiro@users.noreply.github.com>
2023-06-07 11:39:57 -0500

    counter made

---
+++ counter.js
@@ -0,0 +1,14 @@
+const counter = document.getElementById('counter');
+var count = 0;
+
+counter.innerHTML = count;
+
+function plus() {
+	count++;
+	counter.innerHTML = count;
+}
+
+function minus() {
+	count--;
+	counter.innerHTML = count;
+}
